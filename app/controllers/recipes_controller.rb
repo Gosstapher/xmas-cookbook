b1e32da2328190361ab2dd6046334fa055f57fcd
Gosstapher <gosstapher@gmail.com>
2015-12-14 17:08:06 +0000

    everything works so far

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,20 +4,44 @@ class RecipesController <ApplicationController
     @recipes = Recipe.all
   end
 
+  def new
+    @recipe = Recipe.new
+    @category = Category.all
+  end
+
+  def create
+    Recipe.create(recipe_params)
+    redirect_to(recipes_path)
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+    @category = Category.all
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @category = Category.find(params["recipe"]["category_id"].to_i)
+    @category.recipes << @recipe
+    @recipe.update(recipe_params)
+    redirect_to(recipes_path)
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to(recipes_path)
+  end
 
 
 
-
-
-
-
-
-
-
-
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :ingredients, :method, :image, :category_id)
+  end
 
 end
