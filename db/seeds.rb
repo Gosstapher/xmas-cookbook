74aa89c13b4cdae8ce7b5694fb98d4f5304ce07c
Gosstapher <gosstapher@gmail.com>
2015-12-14 14:52:51 +0000

    categories now list recipies with links

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,13 @@
 # Category.create!(title: 'Main Courses')
 # Category.create!(title: 'Desserts')
 
-Recipe.create!(title: "Soup", description: "Hot liquid food", ingredients: "Stock, other things, more things", method: "Boil all the things then blend them", image:"http://www.metalculture.com/wp-content/uploads/Recipe-For-Potage-aux-Legumes-Green-Vegetable-Soup.jpg", category_id: 1)
+# Recipe.create!(title: "Soup", description: "Hot liquid food", ingredients: "Stock, other things, more things", method: "Boil all the things then blend them", image:"http://www.metalculture.com/wp-content/uploads/Recipe-For-Potage-aux-Legumes-Green-Vegetable-Soup.jpg", category_id: 1)
+
+# Recipe.create!(title: "Garlic Bread", description: "Garlic flavoured bread", ingredients: "Garlic, Bread", method: "Combine both Garlic and Bread.", image:"http://www.romaniangarlic.com/wp-content/uploads/2015/02/Garlic_Bread.jpg", category_id: 1)
+
+
+
+
 
 
 
